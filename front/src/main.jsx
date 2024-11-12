e2e71a86371ff8b5c36d46c8cf1b0de0d4c78ef1
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-11-12 13:17:11 +0200

    add route for landing page (frontend)

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <Routes>
+      <Route path="/home" element={<App />} />
+    </Routes>
+  </Router>
+);
