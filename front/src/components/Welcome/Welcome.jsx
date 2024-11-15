79050de70973afbf42c6d98e9e96cc4b2789b90c
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-11-15 12:07:20 +0200

    use cookies in Welcome (frontend)

--- front/src/components/Welcome/Welcome.jsx
+++ front/src/components/Welcome/Welcome.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import './welcome.css';
 
 export default function Welcome() {
     const [isVisible, setIsVisible] = useState(true);
     const [isHidden, setIsHidden] = useState(false);
+    const [cookies, setCookie] = useCookies(['welcomeDismissed']);
+
+    useEffect(() => {
+        console.log('Current cookie value:', cookies.welcomeDismissed);
+        if (cookies.welcomeDismissed === true) {
+            setIsVisible(false);
+            setIsHidden(true);
+        }
+    }, [cookies]);
 
     const handleClick = () => {
         setIsVisible(false);
+        setCookie('welcomeDismissed', true, { path: '/', maxAge: 3600 * 24 });
         setTimeout(() => setIsHidden(true), 500);
     };
 
@@ -15,7 +26,10 @@ export default function Welcome() {
             <div className={`welcome-container ${isVisible ? 'visible' : 'hidden'}`}>
                 <div className="welcome-container--box">
                     <h1>Κάν&apos; το!</h1>
-                    <p>Γίνε μέλος της ομάδας μας και δραστηριοποιήσου μαζί μας σήμερα. Μαζί, θα κάνουμε τα <i>λόγια</i>, <b>πράξη</b>. Σε περιμένουμε στον <a href="#">κοινό μας χώρο</a>.</p>
+                    <p>
+                        Γίνε μέλος της ομάδας μας και δραστηριοποιήσου μαζί μας σήμερα. Μαζί, θα κάνουμε τα <i>λόγια</i>, <b>πράξη</b>. Σε περιμένουμε στον{' '}
+                        <a href="#">κοινό μας χώρο</a>.
+                    </p>
                     <button onClick={handleClick}>Κάν&apos;το Τώρα</button>
                 </div>
             </div>
