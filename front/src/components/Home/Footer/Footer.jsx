5ecc1424391763564fdce4327af781c26361e012
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-11-27 12:37:22 +0200

    Footer boilerplate (frontend)

---
+++ front/src/components/Home/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Footer() {
+    return (
+        <footer>
+            <h1>Test</h1>
+        </footer>
+    )
+}
--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import EventCalendar from "./EventCalendar/EventCalendar";
 import Navbar from "./Navbar/Navbar";
 import PastEvents from "./PastEvents/PastEvents";
 import Vision from "./Vision/Vision";
+import Footer from "./Footer/Footer";
 
 
 export default function Home() {
