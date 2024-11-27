de970c652feaed29575ef51b6c875e3fbb769654
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-11-27 12:16:27 +0200

    vision component (frontend)

--- front/src/components/Home/Vision/Vision.jsx
+++ front/src/components/Home/Vision/Vision.jsx
@@ -2,21 +2,16 @@ import "./Vision.css"
 
 export default function Vision() {
     return (
-        <div className="vision--outer">
+        <div id="orama" className="vision--outer">
             <div className="vision">
-                <svg width="100%" height="100%">
-                    <defs>
-                        <filter id="blur-and-invert">
-                            <feGaussianBlur in="SourceGraphic" stdDeviation="3" />
-                            <feColorMatrix type="saturate" values="1" result="fbSourceGraphic" />
-                            <feColorMatrix in="fbSourceGraphic" values="-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0 " />
-                        </filter>
-                    </defs>
-                    <foreignObject width="100%" height="100%" filter="url(#blur-and-invert)">
-                        {/* <!-- Here goes the content --> */}
-                        <h1>Sanity Check</h1>
-                    </foreignObject>
-                </svg>
+                <div className="vision--inner">
+                    <h1>Όραμα</h1>
+                    <p>
+
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut tempore repudiandae veritatis, dolorum omnis iure eaque. Magni ut perferendis quis minus molestiae animi facere assumenda magnam doloribus maxime, ducimus dolorem? <a href="#">μέλη</a>
+
+                    </p>
+                </div>
             </div>
         </div>
     )
