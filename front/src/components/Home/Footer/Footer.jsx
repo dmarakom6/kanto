1305e96de4a637932327cad2861d66e588e0163b
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-11-28 14:41:44 +0200

    footer and final corrections (frontend)

--- front/src/components/Home/Footer/Footer.jsx
+++ front/src/components/Home/Footer/Footer.jsx
@@ -1,9 +1,10 @@
-
+import './Footer.css'
 
 export default function Footer() {
     return (
-        <footer>
-            <h1>Test</h1>
+        <footer className="footer">
+            <h1 className='footer--title'>Για τον Πολιτισμό, το Περιβάλλον, την Έκφραση, τη Δημιουργία... <span style={{color: 'var(--fg-third)'}}>Κάν΄το!</span></h1>
+            <div className="footer--copyright">&copy;{new Date().getFullYear()} <a href='https://www.instagram.com/dimitrismarako'>Δημήτρης Μαρακομιχελάκης</a>. All Rights Reserved.</div>
         </footer>
     )
 }
--- front/src/components/Home/Vision/Vision.jsx
+++ front/src/components/Home/Vision/Vision.jsx
@@ -8,7 +8,7 @@ export default function Vision() {
                     <h1>Όραμα</h1>
                     <p>
 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut tempore repudiandae veritatis, dolorum omnis iure eaque. Magni ut perferendis quis minus molestiae animi facere assumenda magnam doloribus maxime, ducimus dolorem? <a href="#">μέλη</a>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut tempore repudiandae veritatis, dolorum omnis iure eaque. Magni ut perferendis quis minus molestiae animi facere assumenda magnam doloribus maxime, ducimus dolorem? Επισκεφτείτε μας <a href="#">εδώ</a>.
 
                     </p>
                 </div>
