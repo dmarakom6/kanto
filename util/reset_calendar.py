53245c5e816e1ae53860e7a34724bf3461d5aa7d
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-12-04 10:52:39 +0200

    reset calendar utility (back)

---
+++ util/reset_calendar.py
@@ -0,0 +1,20 @@
+def reset_calendar():
+    try:
+        with open("../front/data/calendar.json", "w") as cal:
+            cal.write("""{
+    "1": {
+        "title": "",
+        "date": "",
+        "category": "",
+        "url": ""
+        }
+}""")
+    
+            return "Calendar reset."
+    
+    except:
+        raise("An Error Occured. Calendar not reset.")
+    
+    
+if __name__ == "__main__":
+    print(reset_calendar())
