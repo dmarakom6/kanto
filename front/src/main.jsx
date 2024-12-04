c660c458eeca6d57384d69fded4b173150f8aa02
Dimitris Marakomihelakis <dmarakom@gmail.com>
2024-12-04 11:23:38 +0200

    remove react router (front)

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </Router>
+  <App />
 );
