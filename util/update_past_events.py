139a9911f5d98a2a06e6dc02a5a7cc5fa8daaaaf
Dimitris Marakomihelakis <dmarakom@gmail.com>
2025-02-21 13:04:49 +0200

    add all past events, set recurring

--- util/update_past_events.py
+++ util/update_past_events.py
@@ -6,7 +6,7 @@ import json
 with open("../front/data/calendar.json", "r") as file:
     existing_events = json.load(file, strict=False)
 
-past_event_info = fetch_past_events(15) #default
+past_event_info = fetch_past_events(100) #default
 
 # Create a set of existing event values for quick comparison, might find a quicker way in the future
 existing_event_values = {json.dumps(event, sort_keys=True) for event in existing_events.values()}
